4ef52f3f4f54d6fa2f010b6e0c04d110eb3387bb
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2025-10-19 22:41:48 +0200

    feat: implemented models for products.

---
+++ svc/server/src/data/models.py
@@ -0,0 +1,35 @@
+from typing import List
+
+from pydantic import BaseModel, Field, RootModel
+
+
+class Product(BaseModel):
+    """Pet food store product."""
+
+    product_name: str
+    product_description: str
+    quantity: int = Field(
+        description="Number of available units in the store. Cannot be negative."
+    )
+    price: float = Field(
+        description="Price for a single unit in the store. Cannot be negative."
+    )
+    active: bool = Field(description="Whether product can be sold.")
+
+
+class ProductWithId(Product):
+    """Pet food store product, but with ID."""
+
+    product_id: int
+
+
+class ProductWithIdList(RootModel):
+    """List of products with ID."""
+
+    root: List[ProductWithId]
+
+
+class NotFoundProduct(BaseModel):
+    """Model for Swagger UI."""
+
+    detail: str = Field(example="Product with id=1123 is not found.")
