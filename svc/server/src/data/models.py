9096aa2b9161bab50f1000a52647ebf05a236a85
Ihor Kostiuk <ihor.kostiuk@knu.ua>
2025-10-20 02:40:31 +0200

    fix: constrains on price and quantity.

--- svc/server/src/data/models.py
+++ svc/server/src/data/models.py
@@ -6,13 +6,13 @@ from pydantic import BaseModel, Field, RootModel
 class Product(BaseModel):
     """Pet food store product."""
 
-    product_name: str
+    product_name: str = Field(max_length=255)
     product_description: str
     quantity: int = Field(
-        description="Number of available units in the store. Cannot be negative."
+        description="Number of available units in the store. Cannot be negative.", ge=0
     )
     price: float = Field(
-        description="Price for a single unit in the store. Cannot be negative."
+        description="Price for a single unit in the store. Cannot be negative.", gt=0
     )
     active: bool = Field(description="Whether product can be sold.")
 
